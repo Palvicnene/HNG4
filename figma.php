e599beecde1f94976ccb0d778cee2014b95496f0
Palvicnene <32469806+Palvicnene@users.noreply.github.com>
2018-04-14 11:24:07 +0100

    Add files via upload

---
+++ figma.php
@@ -0,0 +1,35 @@
+<?php
+
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+	<title>HNG Internship 4</title>
+	<link rel="stylesheet" type="text/css" href="figma.css">
+</head>
+<body>
+	<div class="home">
+		<p class="HNG">HNG INTERNSHIP 4</p>
+	</div>
+	<div class="navbar">
+		<p class="menu">
+			<span style="color: #333;">About Us</span> <span style="color: #828282;">|</span> Packages <span style="color: #828282;">|</span> Contact Us
+		</p>
+	</div>
+	<div class="main">
+		<div class="heading">
+			<p class="intern">HNG INTERNSHIP 4</p>
+		</div>
+		<div class="info">
+			<p class="write">HNG Internship 4 is definitely one that should not to be missed. It helps give the experience of coding and design. <span style="font-style: italic;font-weight: bold;">Avail yourself of this opportunity!</span>
+			</p>
+		</div>
+		<div class="time">
+		<?php echo date("H : i : s"); ?>			
+		</div>
+	</div>
+</body>
+</html>
+
+
